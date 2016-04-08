da34139d7f5d49ca82425da6de0ccff9452c3dd2
ihuanglele <844649263@qq.com>
2016-04-08 10:45:07 +0800

    添加了一些公用的函数

--- App/Common/Common/function.php
+++ App/Common/Common/function.php
@@ -40,3 +40,54 @@ function Mydate($timestr=''){
         return date('Y-m-d H:i', $timestr);
     }
 }
+
+
+/**
+ * @param $openId
+ */
+function getWxUserInfo($openId){
+    $access = getWxAccessToken();
+    $url = "https://api.weixin.qq.com/cgi-bin/user/info?access_token=$access&openid=$openId&lang=zh_CN";
+    $res = myCurl($url);
+    $info = json_decode($res,true);
+    return $info;
+}
+
+
+/**
+ * @return mixed 微信凭证
+ */
+function getWxAccessToken(){
+    $token = S('Wx-access_token');
+    if(!$token){
+        $Wx = C('Wx');
+        $appId = $Wx['AppID'];
+        $appSec = $Wx['AppSecret'];
+        $url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=$appId&secret=$appSec";
+        $res = myCurl($url);
+        $data = json_decode($res,true);
+        $token = $data['access_token'];
+        S('Wx-access_token',$token,$data['expires_in']-1000);
+    }
+    return $token;
+}
+
+
+function myCurl($url,$data=false){
+    $ch = curl_init();
+    //设置超时
+    curl_setopt($ch, CURLOPT_TIMEOUT, 6);
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER,FALSE);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST,FALSE);
+    if($data){
+        curl_setopt_array($ch,$data);
+    }
+    curl_setopt($ch, CURLOPT_HEADER, FALSE);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+
+    //运行curl，结果以jason形式返回
+    $res = curl_exec($ch);
+    curl_close($ch);
+    return $res;
+}
